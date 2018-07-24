4fb271347f167b96e0d022adb38c47522c5d6005
gary80212@gmail.com <gary80212@gmail.com>
2018-07-25 01:49:45 +0800

    create user model using devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_172527) do
+ActiveRecord::Schema.define(version: 2018_07_24_173735) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,30 @@ ActiveRecord::Schema.define(version: 2018_07_23_172527) do
     t.integer "replies_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "role", default: "guest"
+    t.string "company"
+    t.string "title"
+    t.string "intro"
+    t.string "website"
+    t.string "twitter"
+    t.string "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
