aa2b33062b1e4ff04e131898e5d52c0dbae8e7cf
gary80212@gmail.com <gary80212@gmail.com>
2018-07-26 01:52:13 +0800

    add avator upload using carrierwave gem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,10 @@ class QuestionsController < ApplicationController
 
   def index
   end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
+  end
 end
