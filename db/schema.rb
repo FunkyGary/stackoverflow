a97cd14f9bfe1aa0a7faf42b89d7a4eaddbbece8
gary80212@gmail.com <gary80212@gmail.com>
2018-07-26 01:03:09 +0800

    add column avator to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_173735) do
+ActiveRecord::Schema.define(version: 2018_07_25_170034) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_173735) do
     t.string "github"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avator"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
