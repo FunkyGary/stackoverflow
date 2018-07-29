b04ba15f51cc40e17e06ff39aea99051ae07a9d3
gary80212@gmail.com <gary80212@gmail.com>
2018-07-29 23:22:10 +0800

    user can delete answer and question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,27 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: [:create, :destroy]
+
   def create
-      @question = Question.find(params[:question_id])
       @answer = @question.answers.build(answer_params)
       @answer.user = current_user
       @answer.save!
       redirect_to question_path(@question)
   end
 
+  def destroy
+    @answer = Answer.find(params[:id])
+
+    if current_user
+      @answer.destroy
+      redirect_to question_path(@question)
+    end
+  end
+
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:content)
