643ade7c29962a5c9428999b94eea86699776cd2
gary80212@gmail.com <gary80212@gmail.com>
2018-08-22 00:42:38 +0800

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable,
+         :omniauthable, omniauth_providers: [:github]
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -29,5 +31,12 @@ class User < ApplicationRecord
     end
     self.totalupvotes = totalupvotes
     self.save
+  end
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.skip_confirmation!
+    end
   end
 end
